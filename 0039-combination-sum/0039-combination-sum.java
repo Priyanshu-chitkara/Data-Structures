7af662374c532321b861b58676ac92eca9a4b67c
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-07-20 13:36:38 +0530

    Time: 1 ms (99.9%), Space: 44.7 MB (45.24%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
-    public void printS(int[] candidates, int target, List<List<Integer>> result, List<Integer> cur, int ind, int s) {
-        if (s == target) {
-            result.add(new ArrayList<>(cur));
+    public void printF(int [] candidates,int target,int ind,List<List<Integer>> res,List<Integer> cur){
+        int n=candidates.length;
+        if(ind==n){
+            if(target==0){
+                res.add(new ArrayList<>(cur));
+            }
             return;
         }
-        if (ind >= candidates.length || s > target) return;
+        if(candidates[ind]<=target){
+            cur.add(candidates[ind]);
+            printF(candidates,target-candidates[ind],ind,res,cur);
+            cur.remove(cur.size()-1);
+        }
+        printF(candidates,target,ind+1,res,cur);
 
-        // Include current element
-        cur.add(candidates[ind]);
-        printS(candidates, target, result, cur, ind, s + candidates[ind]); // can use same element again
-        cur.remove(cur.size() - 1); // backtrack
-
-        // Skip current element
-        printS(candidates, target, result, cur, ind + 1, s);
     }
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        printS(candidates, target, result, cur, 0, 0);
-        return result;
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> cur=new ArrayList<>();
+        printF(candidates,target,0,res,cur);
+        return res;
+        
     }
 }
