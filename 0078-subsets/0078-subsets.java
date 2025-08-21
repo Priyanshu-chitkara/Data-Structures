8ae2367141f6b68341faa5812a0c18b39d343161
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-08-21 11:11:00 +0530

    Time: 0 ms (100%), Space: 42.4 MB (97.52%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,28 +1,21 @@
 class Solution {
-    public void printf(int [] nums,List<List<Integer>> res, List<Integer> st,int ind){
+    public void printf(int [] nums,List<List<Integer>> res,List<Integer> st,int ind){
         int n=nums.length;
         if(ind==n){
-             res.add(new ArrayList<>(st));
-             return;
+            res.add(new ArrayList<>(st));
+            return;
         }
-          st.add(nums[ind]);
+        st.add(nums[ind]);
         printf(nums,res,st,ind+1);
-      
         st.remove(st.size()-1);
         printf(nums,res,st,ind+1);
-        
-
     }
-    public List<List<Integer>> subsets(int[] nums){
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> st=new ArrayList<>();
         printf(nums,res,st,0);
         return res;
-
-        
-       
-    }
-       
-        
         
     }
+    
+}
