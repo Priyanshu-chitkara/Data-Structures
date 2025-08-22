63e6894e26a06aee0903570095d53a5e9ea25513
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-08-22 20:37:58 +0530

    Time: 0 ms (100%), Space: 41.9 MB (47.15%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        // Start from the last digit and move left
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                // No carry needed: just add 1 and return
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
                 digits[i]++;
                 return digits;
             }
-            // digits[i] == 9 → becomes 0 and carry goes to the next digit to the left
-            digits[i] = 0;
+            digits[i]=0;
         }
-
-        // If we are here, all digits were 9 (e.g., 9, 99, 999 → 10, 100, 1000)
-        int[] res = new int[digits.length + 1];
-        res[0] = 1; // rest are 0 by default
+        
+        int [] res=new int[n+1];
+        res[0]=1;
+        res[n]=0;
         return res;
+        
     }
 }
