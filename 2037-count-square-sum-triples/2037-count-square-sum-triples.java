7033b45dcbcd7ebd439b9c84af519f89c0d133c7
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-08-24 11:57:09 +0530

    Time: 346 ms (9.33%), Space: 40.7 MB (37.5%) - LeetHub

--- 2037-count-square-sum-triples/2037-count-square-sum-triples.java
+++ 2037-count-square-sum-triples/2037-count-square-sum-triples.java
@@ -2,15 +2,15 @@ class Solution {
     public int countTriples(int n) {
         int count=0;
         for(int i=1;i<=n;i++){
-            for(int j=i+1;j<=n;j++){
-                for(int k=j+1;k<=n;k++){
-                    if(Math.pow(i,2)+Math.pow(j,2)==Math.pow(k,2)){
+            for(int j=1;j<=n;j++){
+                for(int k=1;k<=n;k++){
+                    if((i*i) + (j*j)==(k*k)){
                         count++;
                     }
                 }
             }
         }
-        return 2*count;
+        return count;
         
     }
 }
