b647a14abb519b0d2618a8ceb191c152c5a7498b
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-09-06 19:09:41 +0530

    Time: 12 ms (57.69%), Space: 45.9 MB (18.88%) - LeetHub

--- 0946-smallest-range-ii/0946-smallest-range-ii.java
+++ 0946-smallest-range-ii/0946-smallest-range-ii.java
@@ -1,10 +1,11 @@
 class Solution {
     public int smallestRangeII(int[] nums, int k) {
         int n=nums.length;
+         Arrays.sort(nums);
       
        
         int ans=nums[n-1]-nums[0];
-        Arrays.sort(nums);
+
         for(int i=0;i<n-1;i++){
             int max=Math.max(nums[n-1]-k,nums[i]+k);
            int  min=Math.min(nums[0]+k,nums[i+1]-k);
