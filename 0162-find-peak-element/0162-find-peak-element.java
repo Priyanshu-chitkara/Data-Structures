ba7cfb23354b6018b6c29ffc48e0dd39a6b98531
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-09-16 13:38:31 +0530

    Time: 0 ms (100%), Space: 42.5 MB (56.46%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -17,11 +17,11 @@ class Solution {
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
             }
-            else if(nums[mid]>nums[mid+1]){
-                high=mid;
+            else if(nums[mid]>nums[mid-1]){
+                low=mid+1;
             }
             else{
-                low=mid+1;
+                high=mid-1;
             }
         }
         return -1;
