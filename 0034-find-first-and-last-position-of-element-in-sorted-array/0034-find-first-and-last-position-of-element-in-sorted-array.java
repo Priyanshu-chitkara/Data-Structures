c11b9e63c82141a1e333f8866ae5452c45260f66
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-09-24 12:08:03 +0530

    Time: 0 ms (100%), Space: 46 MB (49.28%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,59 +1,52 @@
 class Solution {
-    public int firstPosition(int []nums,int x){
+    public int firstposition(int[] nums,int target){
         int n=nums.length;
         int low=0;
         int high=n-1;
         int index=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]==x){
-                index=mid;
-                high=mid-1;
-
-
-
-                
-            }
-            else if(nums[mid]>x){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            
-            }
-           
+          if(nums[mid]==target){
+            index=mid;
+            high=mid-1;
+          }
+          else if(nums[mid]>target){
+            high=mid-1;
+          }
+          else{
+            low=mid+1;
+          }
         }
         return index;
-
-
     }
-    public int lastPosition(int []nums,int x){
+    public int lastposition(int[] nums,int target){
         int n=nums.length;
         int low=0;
         int high=n-1;
         int index=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]==x){
+            if(nums[mid]==target){
                 index=mid;
                 low=mid+1;
             }
-            else if(nums[mid]>x){
-                high=mid-1;
+            else if(nums[mid]<target){
+                low=mid+1;
+
+
             }
             else{
-                low=mid+1;
+                high=mid-1;
             }
+
         }
         return index;
-
-
     }
     public int[] searchRange(int[] nums, int target) {
-        int [] ans=new int[2];
-        ans[0]=firstPosition(nums,target);
-        ans[1]=lastPosition(nums,target);
-        return ans;
+        int arr[]=new int[2];
+        arr[0]=firstposition(nums,target);
+        arr[1]=lastposition(nums,target);
+        return arr;
         
     }
 }
