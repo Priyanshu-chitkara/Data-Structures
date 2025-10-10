550736f77f8add234be09a31fde3186b37d1fe50
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-10-10 20:11:09 +0530

    Time: 1 ms (96.17%), Space: 42.3 MB (69.63%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,51 +1,35 @@
-// class Solution {
-//     public int maxFrequencyElements(int[] nums) {
-//         int n=nums.length;
-//         int [] freq=new int [n+1];
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int n=nums.length;
+        int [] freq=new int [101];
         
-//         for(int i=0;i<n;i++){
-//             freq[nums[i]]++;
-//         }
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
         
-//         int ans=0;
-//         int res=0;
-//         for(int i=0;i<freq.length;i++){
-//             ans=Math.max(ans,freq[i]);
+        int ans=0;
+        int res=0;
+        for(int i=0;i<freq.length;i++){
+            ans=Math.max(ans,freq[i]);
             
             
 
-//         }
-//         int count=0;
-//         for(int i=0;i<freq.length;i++){
-//             if(freq[i]==ans){
-//                 count++;
+        }
+        int count=0;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==ans){
+                count++;
 
-//             }
-//         }
+            }
+        }
 
             
             
-//          return count*ans;    
-//         }
+         return count*ans;    
+        }
        
         
         
         
-//     }
-class Solution {
-    public int maxFrequencyElements(int[] nums) {
-        int[] freq = new int[101];
-        for(int num : nums){
-            freq[num]++;
-        }
-        int max=-1;
-        for(int fre : freq){
-            if(max<fre) max=fre;
-        }
-        int c=0;
-        for(int fre : freq){
-            if(max==fre) c++;
-        }
-        return c*max;
     }
-}
+
