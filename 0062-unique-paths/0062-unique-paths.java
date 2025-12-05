05aa3881ade2b93e80ca361f6ef2d9b638c291bb
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-12-05 12:51:23 +0530

    Time: 1 ms (10.96%), Space: 42.1 MB (22.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,44 @@
 class Solution {
-    public int f(int [][] arr,int i,int j,int m,int n,int [][] dp){
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(i>m || j>n){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int right=f(arr,i,j+1,m,n,dp);
-        int bottom=f(arr,i+1,j,m,n,dp);
-        return  dp[i][j]=right+bottom;
-    }
+    // public int f(int [][] arr,int i,int j,int m,int n,int [][] dp){
+    //     if(i==m-1 && j==n-1){
+    //         return 1;
+    //     }
+    //     if(i>m || j>n){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //      int right=f(arr,i,j+1,m,n,dp);
+    //     int bottom=f(arr,i+1,j,m,n,dp);
+    //     return  dp[i][j]=right+bottom;
+    // }
     public int uniquePaths(int m, int n) {
-        int [][] arr=new int [m][n];
-        int [][] dp=new int[m+1][n+1];
-        for(int [] row: dp){
-            Arrays.fill(row,-1);
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                } 
+                int right=0;
+                int bottom=0;
+                if(i>0){
+                    right=dp[i-1][j];
+
+                }
+                if(j>0){
+                    bottom=dp[i][j-1];
+                }
+                dp[i][j]=right+bottom;
+
+            }
+            
         }
-       int res=f(arr,0,0,m,n,dp);
-       return res;
-        
+
+        return dp[m-1][n-1];
+
     }
 }
