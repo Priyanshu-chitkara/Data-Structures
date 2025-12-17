e38eb50d99ea7242987d86858fdf8599996463a1
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-12-17 16:22:57 +0530

    Time: 2 ms (85.39%), Space: 47.7 MB (68.48%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,28 +1,28 @@
 class Solution {
-    public void dfs(boolean [] arr,int[][] graph,int ind,List<Integer> ls,List<List<Integer>> res){
-        int n=graph.length;
+    public void dfs(List<List<Integer>> res,boolean[] arr,List<Integer> ls,int ind,int[][] graph){
          arr[ind]=true;
-         ls.add(ind);
+        ls.add(ind);
+        int n=graph.length;
         if(ind==n-1){
             res.add(new ArrayList<>(ls));
         }
+       
         for(int neigh:graph[ind]){
             if(!arr[neigh]){
-                dfs(arr,graph,neigh,ls,res);
+                dfs(res,arr,ls,neigh,graph);
             }
+
         }
         ls.remove(ls.size()-1);
         arr[ind]=false;
+
     }
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> res=new ArrayList<>();
+        List<Integer> ls=new ArrayList<>();
         int n=graph.length;
         boolean [] arr=new boolean[n];
-    
-                    List<Integer> ls=new ArrayList<>();
-
-            dfs(arr,graph,0,ls,res);
-        
+        dfs(res,arr,ls,0,graph);
         return res;
         
     }
