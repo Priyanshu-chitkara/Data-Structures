5f671999b45329c27d80eef04ad846afc68097ef
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-12-22 20:58:53 +0530

    Time: 1 ms (99.73%), Space: 50.2 MB (40.25%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,27 +1,28 @@
 class Solution {
-    public int f(int[][] grid,int m,int n,int[][] dp){
-        if(m<0 || n<0){
+    public int f(int row,int col,int [][] grid,int [][] dp){
+        if(row==0 && col==0){
+            return grid[row][col];
+        }
+         if(row<0 || col<0){
             return Integer.MAX_VALUE;
         }
-        if(m==0 && n==0){
-            return grid[0][0];
+        if(dp[row][col]!=-1){
+            return dp[row][col];
         }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        int topleft=f(grid,m,n-1,dp);
-        int bottomright=f(grid,m-1,n,dp);
-        return dp[m][n]=grid[m][n] + Math.min(topleft,bottomright);
+        int left =  f(row,col-1,grid,dp);
+        int up =  f(row-1,col,grid,dp);
+        return dp[row][col]=grid[row][col] + Math.min(left,up);
     }
     public int minPathSum(int[][] grid) {
+        
         int m=grid.length;
         int n=grid[0].length;
-        int[][] dp=new int[m][n];
+        int [][] dp=new int [m][n];
         for(int [] arr:dp){
             Arrays.fill(arr,-1);
         }
-       int ans=f(grid,m-1,n-1,dp);
-       return ans;
+        int ans=f(m-1,n-1,grid,dp);
+        return ans;
         
     }
 }
