32fea18051191fa951a0461e561d4ab50b7a493c
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-12-23 16:45:00 +0530

    Time: 3 ms (83.55%), Space: 45.5 MB (49.15%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,20 +1,34 @@
-//tabulation
 class Solution {
+    // public int f(List<List<Integer>> triangle,int row,int col){
+    //     if(row==0 && col==0){
+    //         return triangle.get(row).get(col);
+    //     }
+    //     if(row<0 || col<0){
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     int up=f(triangle,row-1,col-1);
+    //     int dig=f(triangle,row,col-1);
+    //     return triangle.get(row).get(col)+Math.min(up,dig);
+    // }
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n=triangle.size();
-        int[][] dp=new int[n][n];
-        for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle.get(n-1).get(j);
+        int m=triangle.size();
+        int ans=0;
+        int [][] dp=new int[m][m];
+     for(int i=m-1;i>=0;i--){
+        dp[m-1][i]=triangle.get(m-1).get(i);
+        
+
+     }
+    for(int i=m-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            int dwn=dp[i+1][j];
+            int dig=dp[i+1][j+1];
+            dp[i][j]=triangle.get(i).get(j)+Math.min(dwn,dig);
         }
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int dpp=triangle.get(i).get(j)+dp[i+1][j];
-                int dg=triangle.get(i).get(j)+dp[i+1][j+1];
-                dp[i][j]=Math.min(dpp,dg);
-            }
-            
-        }
-        return dp[0][0];
+        
+    }
+   
+    return dp[0][0];
 
         
     }
