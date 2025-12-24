71f2af7d80ce3626f88dec5a3774fc7ed898413d
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2025-12-24 13:14:23 +0530

    Time: 2 ms (89.97%), Space: 45.4 MB (7.95%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,35 +1,24 @@
 class Solution {
-    public void f(List<List<Integer>> res,List<Integer> ls,int n,int [] nums,int ind){
-        if(ind==n){
-            if(res.indexOf(ls)==-1){
-                res.add(new ArrayList<>(ls));
+    public void f(int [] nums,int ind,List<List<Integer>> res,List<Integer> ls,int n){
+        res.add(new ArrayList<>(ls));
+
+        for(int i=ind;i<n;i++){
+            if(i>ind && nums[i]==nums[i-1]) continue;
+            ls.add(nums[i]);
+            f(nums,i+1,res,ls,n);
+            ls.remove(ls.size()-1);
             
 
-            
-
-            }
-           
-            
-
-            return;
-            
         }
-        ls.add(nums[ind]);
-        f(res,ls,n,nums,ind+1);
-        ls.remove(ls.size()-1);
-        f(res,ls,n,nums,ind+1);
-
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-       
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> ls=new ArrayList<>();
         int n=nums.length;
-        f(res,ls,n,nums,0);
-
+        f(nums,0,res,ls,n);
         return res;
-        
+
         
     }
 }
