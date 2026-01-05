079ec48f82265e181d9f22408642b452ab15f3b2
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-01-05 15:55:10 +0530

    Time: 0 ms (100%), Space: 45.2 MB (14.47%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -18,14 +18,13 @@ class Solution {
         if(root==null){
             return false;
         }
-        if(root!=null && root.left==null && root.right==null){
+        if(root!=null && root.right==null && root.left==null){
             if(targetSum==root.val){
-            return true;
+                return true;
+            }
+            return false;
         }
-        
-
-        }
-        
         return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+        
     }
 }
