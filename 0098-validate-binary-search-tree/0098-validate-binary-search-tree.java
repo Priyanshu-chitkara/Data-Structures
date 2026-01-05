27f5655a8fc880a7f8c7e250d57ef312e44f89ea
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-01-05 15:46:13 +0530

    Time: 2 ms (13.61%), Space: 45.9 MB (5.66%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,13 +14,21 @@
  * }
  */
 class Solution {
-    public boolean checkbst(TreeNode root,Long min,Long max){
-        if(root==null) return true;
-        if(root.val<=min || root.val>=max) return false;
-        return checkbst(root.left,min,(long) root.val) && checkbst(root.right,(long) root.val,max);
+    public void inorder(TreeNode root,List<Integer> ls){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,ls);
+        ls.add(root.val);
+        inorder(root.right,ls);
     }
     public boolean isValidBST(TreeNode root) {
-        return checkbst(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        List<Integer> ls=new ArrayList<>();
+        inorder(root,ls);
+        for(int i=1;i<ls.size();i++){
+            if(ls.get(i)<=ls.get(i-1)) return false;
+        }
+        return true;
         
     }
 }
