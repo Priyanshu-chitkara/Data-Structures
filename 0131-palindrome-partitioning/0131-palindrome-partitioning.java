a82d0013ff0db18c02bcbc26bae10acb0434a32b
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-01-22 12:12:17 +0530

    Time: 8 ms (72.73%), Space: 65.2 MB (81.66%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,33 +1,35 @@
 class Solution {
-    public void f(List<List<String>> res,List<String> ls,String s,int ind){
-        if(ind==s.length()){
+    public boolean ispal(String s,int start,int end){
+        while(start<=end){
+            if(s.charAt(start++)!=s.charAt(end--)){
+                return false;
+            }
+
+
+        }
+        return true;
+    }
+    public void f(List<List<String>> res,List<String> ls,String s,int n,int ind){
+        if(ind==n){
             res.add(new ArrayList<>(ls));
             return;
         }
-        for(int i=ind;i<s.length();i++){
-            if(ispall(s,ind,i)){
+        for(int i=ind;i<n;i++){
+            if(ispal(s,ind,i)){
                 ls.add(s.substring(ind,i+1));
-                f(res,ls,s,i+1);
+                f(res,ls,s,n,i+1);
                 ls.remove(ls.size()-1);
 
             }
+            
         }
-        
-    }
-    public boolean ispall(String s,int start,int end){
-        while(start<=end){
-           if(s.charAt(start++)!=s.charAt(end--)){
-            return false;
-           }
-          
-        }
-         return true;
 
     }
     public List<List<String>> partition(String s) {
+        int n=s.length();
         List<List<String>> res=new ArrayList<>();
         List<String> ls=new ArrayList<>();
-        f(res,ls,s,0);
+        f(res,ls,s,n,0);
         return res;
         
     }
