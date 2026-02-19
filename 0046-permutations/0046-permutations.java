1dc248d4e4f73eb9919ad6ec1653552c9383ba97
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-02-19 19:40:56 +0530

    Time: 1 ms (85.9%), Space: 45.6 MB (46.55%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,36 +1,39 @@
 class Solution {
-    public void f(List<List<Integer>> res,List<Integer> ls,int[] nums,int n,int ind){
+    public void swap(int [] nums,int first,int second){
+        int temp=nums[first];
+        nums[first]=nums[second];
+        nums[second]=temp;
+
+
+    }
+    public void f(int[] nums,List<List<Integer>> ls,List<Integer> rs,int ind,int n){
+       
         if(ind==n){
-            res.add(new ArrayList<>(ls));
+            ls.add(new ArrayList<>(rs));
             return;
         }
         for(int i=ind;i<n;i++){
-            isperm(nums,i,ind);
-            ls.add(nums[ind]);
-            f(res,ls,nums,n,ind+1);
-            ls.remove(ls.size()-1);
-            isperm(nums,ind,i);
+            swap(nums,i,ind);
+            rs.add(nums[ind]);
+            
+            
+            f(nums,ls,rs,ind+1,n);
+            
+            
+            
+            rs.remove(rs.size()-1);
+            swap(nums,ind,i);
             
 
-            
-            
-            
         }
-    }
-    public void isperm(int [] nums,int i,int ind){
-        
-        int temp=nums[i];
-        nums[i]=nums[ind];
-        nums[ind]=temp;
-        
         
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> ls=new ArrayList<>();
+        List<List<Integer>> ls=new ArrayList<>();
+        List<Integer> rs=new ArrayList<>();
         int n=nums.length;
-        f(res,ls,nums,n,0);
-        return res;
+        f(nums,ls,rs,0,n);
+        return ls;
         
     }
 }
