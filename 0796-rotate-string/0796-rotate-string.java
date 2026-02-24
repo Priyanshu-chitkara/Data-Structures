8709efbab5f92c08caac9af9950f897dd0991248
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-02-24 23:41:22 +0530

    Time: 3 ms (8.58%), Space: 43.5 MB (8.88%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,7 +3,16 @@ class Solution {
         if(s.length()!=goal.length()){
             return false;
         }
-        return (s+s).contains(goal);
+        String curr=s;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            curr=curr.substring(1)+curr.charAt(0);
+            if(curr.equals(goal)){
+                return true;
+            }
+        }
+        return false;
+
         
     }
 }
