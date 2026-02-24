02f03162ab78a12b2ab1f4a3f89d33454bb01687
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-02-24 23:53:34 +0530

    Time: 7 ms (31.12%), Space: 48.1 MB (39%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int xor=0;
-        for(int ele:nums){
-            xor=xor^ele;
+        int [] ans=new int[2];
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
-        int pos=0;
-        for(int i=0;i<32;i++){
-            if(((xor>>i)&1)==1){
-                pos=i;
-                break;
+        int i=0;
+        for(int key:mp.keySet()){
+            if(mp.get(key)!=2 && i<2){
+                ans[i]=key;
+                i++;
+
             }
         }
-        int a=0;
-        int b=0;
-        for(int ele:nums){
-            if(((ele>>pos)&1)==1){
-                a=a^ele;
-            }
-            else{
-                b=b^ele;
-            }
-        }
-        return new int[]{a,b};
+        return ans;
         
     }
 }
