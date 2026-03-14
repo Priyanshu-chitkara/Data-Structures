77a5b3ef33b9df5eb51fe796d2fb669f6fc6b085
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-03-14 12:37:47 +0530

    Time: 53 ms (79.18%), Space: 70.7 MB (38.59%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,33 +1,29 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        int n=fruits.length;
+        int l=0;
+        int r=0;
+        int ans=0;
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        while(r<n){
+           
+            mp.put(fruits[r],mp.getOrDefault(fruits[r],0)+1);
+            while(mp.size()>2){
+               mp.put(fruits[l],mp.get(fruits[l])-1);
+               if(mp.get(fruits[l])==0){
+                 mp.remove(fruits[l]);
 
-        int n = fruits.length;
-        int l = 0;
-        int r = 0;
-        int ans = 0;
+               }
+              
+               l++;
+             
 
-        HashMap<Integer,Integer> mp = new HashMap<>();
-
-        while(r < n){
-
-            mp.put(fruits[r], mp.getOrDefault(fruits[r],0) + 1);
-
-            while(mp.size() > 2){
-
-                mp.put(fruits[l], mp.get(fruits[l]) - 1);
-
-                if(mp.get(fruits[l]) == 0){
-                    mp.remove(fruits[l]);
-                }
-
-                l++;
             }
-
-            ans = Math.max(ans, r - l + 1);
-
+             int wind_size=r-l+1;
+            ans=Math.max(ans,wind_size);
             r++;
         }
-
         return ans;
+        
     }
 }
