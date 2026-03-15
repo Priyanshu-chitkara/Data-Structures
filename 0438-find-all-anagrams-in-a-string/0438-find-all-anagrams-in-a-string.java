f4994869900da2ca8c6aab3d25e6c1124786eb2e
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-03-15 13:49:39 +0530

    Time: 10 ms (83.8%), Space: 46.5 MB (94.56%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,37 +1,26 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans = new ArrayList<>();
-        int n = s.length(), m = p.length();
-        if (n < m) return ans;
-
-        HashMap<Character, Integer> pMap = new HashMap<>();
-        HashMap<Character, Integer> sMap = new HashMap<>();
-
-        for (char c : p.toCharArray()) {
-            pMap.put(c, pMap.getOrDefault(c, 0) + 1);
+        List<Integer> ls=new ArrayList<>();
+        int [] freqs=new int[26];
+        int [] freqp=new int[26];
+        int l=s.length();
+        int r=p.length();
+        for(int i=0;i<r;i++){
+            freqp[p.charAt(i)-'a']++;
         }
-
-        // first window
-        for (int i = 0; i < m; i++) {
-            char c = s.charAt(i);
-            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        for(int i=0;i<l;i++){
+            freqs[s.charAt(i)-'a']++;
+        
+        if(i>=p.length()){
+            freqs[s.charAt(i-p.length())-'a']--;
         }
-
-        if (sMap.equals(pMap)) ans.add(0);
-
-        for (int i = m; i < n; i++) {
-            char add = s.charAt(i);
-            char remove = s.charAt(i - m);
-
-            sMap.put(add, sMap.getOrDefault(add, 0) + 1);
-
-            sMap.put(remove, sMap.get(remove) - 1);
-            if (sMap.get(remove) == 0) sMap.remove(remove);
-
-            if (sMap.equals(pMap)) {
-                ans.add(i - m + 1);
-            }
+        if(Arrays.equals(freqs,freqp)){
+            ls.add(i-p.length()+1);
         }
-        return ans;
+       
+        }
+         return ls;
+
+        
     }
 }
