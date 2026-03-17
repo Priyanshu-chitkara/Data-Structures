8c6a26da59d5f93c64f16ff30a57393bf502ec6b
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-03-17 12:46:19 +0530

    Time: 17 ms (51.01%), Space: 47.8 MB (24.28%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,50 +1,53 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n=s.length();
-        int m=t.length();
-        int count=m;
-       
-        if(n<m){
+        int slen=s.length();
+        int tlen=t.length();
+        if(tlen>slen){
             return "";
-
+        }
+        HashMap<Character,Integer> mp=new HashMap<>();
+        for(int i=0;i<tlen;i++){
+            mp.put(t.charAt(i),mp.getOrDefault(t.charAt(i),0)+1);
         }
         int l=0;
         int r=0;
         int start=0;
-        int minlen=Integer.MAX_VALUE;
-       HashMap<Character,Integer> mp=new HashMap<>();
-       for(int i=0;i<m;i++){
-        mp.put(t.charAt(i),mp.getOrDefault(t.charAt(i),0)+1);
-       }
-       while(r<n){
-        if(mp.containsKey(s.charAt(r))){
-            mp.put(s.charAt(r),mp.get(s.charAt(r))-1);
-            if(mp.get(s.charAt(r))>=0){
-                count--;
+        int cnt=tlen;
+        int ans=0;
+        int end=Integer.MAX_VALUE;
+        
+        while(r<slen){
+            if(mp.containsKey(s.charAt(r))){
+                mp.put(s.charAt(r),mp.get(s.charAt(r))-1);
+                if(mp.get(s.charAt(r))>=0){
+                     cnt--;
 
-            }
-        }
-        while(count==0){
-            if(r-l+1<minlen){
-                minlen=r-l+1;
-                start=l;
-            }
-            if(mp.containsKey(s.charAt(l))){
-                mp.put(s.charAt(l),mp.get(s.charAt(l))+1);
-                if(mp.get(s.charAt(l))>0){
-                    count++;
                 }
+               
+
             }
-            l++;
+            while(cnt==0){
+                if(r-l+1<end){
+                    end=r-l+1;
+                    start=l;
+
+                }
+                if(mp.containsKey(s.charAt(l))){
+                    mp.put(s.charAt(l),mp.get(s.charAt(l))+1);
+                    if(mp.get(s.charAt(l))>0){
+                        cnt++;
+                    }
+                }
+                l++;
+
+            }
+           
+            
+            
+            r++;
         }
-       
-    
-
-        r++;
-       }
-       if(minlen==Integer.MAX_VALUE) return "";
-       return s.substring(start,start+minlen);
-
+        if(end==Integer.MAX_VALUE) return "";
+        return s.substring(start,start+end);
         
     }
 }
