4992fb3b94046dcf1d5240267ecb6bec659245ed
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-03-18 19:27:43 +0530

    Time: 1 ms (99.88%), Space: 77.2 MB (79.59%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //kadane's algo
         int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
         int sum=0;
         int ans=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-            
-            ans=Math.max(ans,sum);
             if(sum<0){
                 sum=0;
             }
+            sum+=nums[i];
+            
+            ans=Math.max(ans,sum);
 
         }
         return ans;
