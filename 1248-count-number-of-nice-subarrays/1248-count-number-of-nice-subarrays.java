a7e78d1d7dde7dfcb9860e890747be2e33eea05c
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-03-19 13:57:02 +0530

    Time: 13 ms (69.51%), Space: 59.6 MB (61.64%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,31 +1,32 @@
 class Solution {
-    public int cnt(int [] nums,int k){
-         int n=nums.length;
-        int l=0;
-        int r=0;
+    public int subarr(int[] nums,int k){
         int ans=0;
         int cnt=0;
+        int l=0;
+        int r=0;
+        int n=nums.length;
+        // HashMap<Integer,Integer> mp=new HashMap<>();
         while(r<n){
-            if(nums[r]%2!=0){
-                cnt++;
-            }
-            while(cnt>k){
-                if(nums[l]%2!=0){
-                    cnt--;
-                }
-                l++;
-            }
-            ans+=r-l+1;
+            // mp.put(nums[r],mp.getOrDefault(nums[r],0)+1);
+           if(nums[r]%2!=0){
+            cnt++;
 
+           }
+           while(cnt>k){
+            if(nums[l]%2!=0){
+                cnt--;
+            }
+            l++;
+           }
+           ans+=r-l+1;
 
+            
             r++;
         }
         return ans;
-
-
     }
     public int numberOfSubarrays(int[] nums, int k) {
-        return cnt(nums,k)-cnt(nums,k-1);
+         return subarr(nums,k)-subarr(nums,k-1);
         
     }
 }
