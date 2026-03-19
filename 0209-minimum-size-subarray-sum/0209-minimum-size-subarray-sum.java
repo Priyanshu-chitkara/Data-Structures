1c04c280e1129a6f69cc3e3a10264b3881923ae1
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-03-19 15:49:18 +0530

    Time: 1 ms (99.84%), Space: 62.1 MB (66.98%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int min_windsize=Integer.MAX_VALUE;
-        int currwindsum=0;
-        int wind_start=0;
-        for(int windend=0;windend<nums.length;windend++){
-            currwindsum+=nums[windend];
+        int ans=Integer.MAX_VALUE;
+        int l=0;
+        int r=0;
+        int n=nums.length;
+        int s=0;
+        while(r<n){
+            s+=nums[r];
 
-             while(currwindsum>=target){
-                min_windsize=Math.min(min_windsize,windend-wind_start+1);
-                currwindsum-=nums[wind_start];
-                wind_start++;
-             }
+            while(s>=target){
+                ans=Math.min(ans,r-l+1);
+                s-=nums[l];
+                l++;
+            }
+            
+            
+            
+            
+            r++;
+
         }
-        if(min_windsize==Integer.MAX_VALUE){
+        if(ans==Integer.MAX_VALUE){
             return 0;
         }
-        else{
-            return min_windsize;
+        return ans;
 
-        }
-        
-       
         
     }
 }
