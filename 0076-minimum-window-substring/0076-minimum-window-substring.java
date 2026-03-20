993ccab32dbeefb2f9551539aa5719d1f35918ec
Priyanshu <156740950+Priyanshu-chitkara@users.noreply.github.com>
2026-03-20 14:34:53 +0530

    Time: 17 ms (50.99%), Space: 47.2 MB (48.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,37 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int slen=s.length();
-        int tlen=t.length();
-        if(tlen>slen){
+        int n=s.length();
+        int m=t.length();
+        if(m>n){
             return "";
         }
         HashMap<Character,Integer> mp=new HashMap<>();
-        for(int i=0;i<tlen;i++){
+        for(int i=0;i<m;i++){
             mp.put(t.charAt(i),mp.getOrDefault(t.charAt(i),0)+1);
         }
-        int l=0;
         int r=0;
+        int l=0;
+        int cnt=m;
         int start=0;
-        int cnt=tlen;
-        int ans=0;
         int end=Integer.MAX_VALUE;
-        
-        while(r<slen){
+        while(r<n){
             if(mp.containsKey(s.charAt(r))){
                 mp.put(s.charAt(r),mp.get(s.charAt(r))-1);
                 if(mp.get(s.charAt(r))>=0){
-                     cnt--;
-
+                    cnt--;
                 }
-               
-
             }
+            
             while(cnt==0){
                 if(r-l+1<end){
-                    end=r-l+1;
-                    start=l;
-
-                }
+                start=l;
+                end=r-l+1;
+            }
                 if(mp.containsKey(s.charAt(l))){
                     mp.put(s.charAt(l),mp.get(s.charAt(l))+1);
                     if(mp.get(s.charAt(l))>0){
@@ -41,12 +36,12 @@ class Solution {
                 l++;
 
             }
-           
-            
-            
             r++;
+
         }
-        if(end==Integer.MAX_VALUE) return "";
+        if(end==Integer.MAX_VALUE){
+            return "";
+        }
         return s.substring(start,start+end);
         
     }
